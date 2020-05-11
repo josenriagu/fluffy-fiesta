f4a5cf5400127d6c216eeb824d411cdfc6e75d98
Josemaria Nriagu <49484425+josenriagu@users.noreply.github.com>
2020-05-11 22:28:17 +0100

    feat(python): solve kangaroo

--- _hackerrrank/kangaroo.py
+++ _hackerrrank/kangaroo.py
@@ -5,14 +5,28 @@ import re
 import sys
 
 # Complete the kangaroo function below.
+
+
 def kangaroo(x1, v1, x2, v2):
-    '''
-    In order for the Kangaroos to meet at a point, either (not both) of the distance or jump should be greater or equal to the coresponding kangaroo
-    Second condition to be met is that the distance + jump must be multiples of each other
-    '''
-    if ((x1 > x2 and v1 < v2) or (x2 > x1 and v2 < v1) or (x2 == x1 and v1 == v2)):
-        if (x1 + v1)%(x2 + v2) == 0 or (x2 + v2)%(x1 + v1) == 0:
-            return "YES"
-        else:
-            return "NO"
-    return "NO"
+    if x2 > x1 and (v2 > v1 or v2 == v1):
+        return "NO"
+    elif x2 > x1 and v1 > v2:
+        cur_first = x1
+        cur_second = x2
+        met = False
+        while not met:
+            cur_first += v1
+            cur_second += v2
+            if cur_first == cur_second:
+                met = True
+            if cur_first > cur_second:
+                return "NO"
+    return "YES"
+
+
+print(kangaroo(0, 3, 4, 2))
+print(kangaroo(0, 2, 5, 3))
+print(kangaroo(14, 4, 98, 2))
+print(kangaroo(42, 3, 94, 2))
+print(kangaroo(21, 6, 47, 3))
+print(kangaroo(43, 2, 70, 2))
